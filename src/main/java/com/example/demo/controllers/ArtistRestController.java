b64b961afcdb4cdc242e599ea42bd9cebad0ffa2
bilalayazqureshi <bilalayazqureshi@localhost>
2025-08-21 15:26:25 +0200

    Passing test for REST GET

--- src/main/java/com/example/demo/controllers/ArtistRestController.java
+++ src/main/java/com/example/demo/controllers/ArtistRestController.java
@@ -1,13 +1,18 @@
 package com.example.demo.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.Artist;
+
 @RestController
-@RequestMapping("/api/artists")
 public class ArtistRestController {
 
-	
+	@GetMapping("/api/artists")
+	public List<Artist> allArtists() {
+		return Collections.emptyList();
 	}
-
-	
+}
--- src/main/java/com/example/demo/controllers/ArtworkRestController.java
+++ src/main/java/com/example/demo/controllers/ArtworkRestController.java
@@ -1,10 +1,17 @@
 package com.example.demo.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/artworks")
-public class ArtworkRestController {
+import com.example.demo.model.Artwork;
 
+@RestController
+public class ArtworkRestController {
+	@GetMapping("/api/artworks")
+	public List<Artwork> allArtworks() {
+		return Collections.emptyList();
+	}
 }
