9180f9ee705a8d86752335df8b842e3b73b0ad96
bilalayazqureshi <bilalayazqureshi@localhost>
2025-08-22 11:39:12 +0200

    Fixed error

--- src/test/java/com/example/demo/controllers/ArtistRestControllerTest.java
+++ src/test/java/com/example/demo/controllers/ArtistRestControllerTest.java
@@ -4,26 +4,23 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
 @WebMvcTest(controllers = ArtistRestController.class)
-public class ArtistRestControllerTest {
+class ArtistRestControllerTest {
 
-	@Autowired
-	private MockMvc mvc;
+    @Autowired
+    private MockMvc mvc;
 
-	@Test
-	public void testAllArtistsEmpty() throws Exception {
-		this.mvc.perform(get("/api/artists")
-				.accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().json("[]"));
-	}
+    @Test
+    void testAllArtistsEmpty() throws Exception {
+        mvc.perform(get("/api/artists")
+                .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().json("[]"));
+    }
 }
--- src/test/java/com/example/demo/controllers/ArtworkRestControllerTest.java
+++ src/test/java/com/example/demo/controllers/ArtworkRestControllerTest.java
@@ -5,26 +5,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-
-
-@RunWith(SpringRunner.class)
 @WebMvcTest(controllers = ArtworkRestController.class)
-public class ArtworkRestControllerTest {
+class ArtworkRestControllerTest {
 
-	@Autowired
-	private MockMvc mvc;
+    @Autowired
+    private MockMvc mvc;
 
-	@Test
-	public void testAllArtworksEmpty() throws Exception {
-		this.mvc.perform(get("/api/artworks").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().json("[]")); 
-	}
+    @Test
+    void testAllArtworksEmpty() throws Exception {
+        mvc.perform(get("/api/artworks")
+                .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().json("[]"));
+    }
 }
