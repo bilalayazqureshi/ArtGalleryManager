4613fa658d4237561c74731badcc650fc6458114
bilalayazqureshi <bilalayazqureshi@localhost>
2025-08-23 13:06:07 +0200

    Passing test for create, update and delete

--- src/main/java/com/example/demo/controllers/ArtistRestController.java
+++ src/main/java/com/example/demo/controllers/ArtistRestController.java
@@ -3,7 +3,14 @@ package com.example.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Artist;
 import com.example.demo.service.ArtistService;
@@ -23,5 +30,20 @@ public class ArtistRestController {
 	@GetMapping("/{id}")
 	public Artist artist(@PathVariable long id) {
 		return artistService.getArtistById(id);
+	}
+
+	@PostMapping("/new")
+	public Artist newArtist(@RequestBody Artist artist) {
+		return artistService.insertNewArtist(artist);
+	}
+
+	@PutMapping("/{id}")
+	public Artist updateArtist(@PathVariable long id, @RequestBody Artist replacement) {
+		return artistService.updateArtistById(id, replacement);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteArtist(@PathVariable long id) {
+		artistService.deleteArtistById(id);
 	}
 }
--- src/main/java/com/example/demo/controllers/ArtworkRestController.java
+++ src/main/java/com/example/demo/controllers/ArtworkRestController.java
@@ -3,7 +3,14 @@ package com.example.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Artwork;
 import com.example.demo.service.ArtworkService;
@@ -21,7 +28,22 @@ public class ArtworkRestController {
 	}
 
 	@GetMapping("/{id}")
-	public Artwork artwork(@PathVariable long id) {
+	public Artwork getArtwork(@PathVariable long id) {
 		return artworkService.getArtworkById(id);
+	}
+
+	@PostMapping("/new")
+	public Artwork create(@RequestBody Artwork artwork) {
+		return artworkService.insertNewArtwork(artwork);
+	}
+
+	@PutMapping("/{id}")
+	public Artwork update(@PathVariable long id, @RequestBody Artwork replacement) {
+		return artworkService.updateArtworkById(id, replacement);
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		artworkService.deleteArtworkById(id);
 	}
 }
