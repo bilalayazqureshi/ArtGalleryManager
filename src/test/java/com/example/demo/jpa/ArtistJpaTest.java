578507fde8dfdbf7b40446e79d932e2270879f8a
bilalayazqureshi <bilalayazqureshi@localhost>
2025-08-23 16:37:25 +0200

    JPA passing test

--- src/test/java/com/example/demo/jpa/ArtistJpaTest.java
+++ src/test/java/com/example/demo/jpa/ArtistJpaTest.java
@@ -1,13 +1,15 @@
 package com.example.demo.jpa;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-
+import org.springframework.test.context.junit4.SpringRunner;
 import com.example.demo.model.Artist;
 
 @DataJpaTest
+@RunWith(SpringRunner.class)
 public class ArtistJpaTest {
 
 	@Autowired
@@ -15,6 +17,6 @@ public class ArtistJpaTest {
 
 	@Test
 	public void testJpaMapping() {
-		Artist saved = entityManager.persistFlushFind(new Artist(null, "Test Artist", "Test Nationality"));
+		Artist saved = entityManager.persistFlushFind(new Artist(null, "Pablo Picasso", "Spanish"));
 	}
 }
