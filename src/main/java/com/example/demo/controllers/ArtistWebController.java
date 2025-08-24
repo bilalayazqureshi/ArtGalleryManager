bb0779f54c3692788f4b7800c82420d39862be43
bilalayazqureshi <bilalayazqureshi@localhost>
2025-08-24 11:08:47 +0200

    Passing test for web

--- src/main/java/com/example/demo/controllers/ArtistWebController.java
+++ src/main/java/com/example/demo/controllers/ArtistWebController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ArtistWebController {
 
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
 }
