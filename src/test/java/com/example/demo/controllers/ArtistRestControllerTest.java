58625e79e33daa099562c7c50e3f5291b2a5cd08
bilalayazqureshi <bilalayazqureshi@localhost>
2025-08-30 18:46:26 +0200

    Integration tests for RestController

--- src/test/java/com/example/demo/controllers/ArtistRestControllerTest.java
+++ src/test/java/com/example/demo/controllers/ArtistRestControllerTest.java
@@ -32,7 +32,7 @@ public class ArtistRestControllerTest {
 	private ArtistService artistService;
 
 	@Test
-	public void testCreateArtist() throws Exception {
+	void testCreateArtist() throws Exception {
 		when(artistService.insertNewArtist(any(Artist.class))).thenReturn(new Artist(3L, "Bob Brown", "American"));
 
 		String newArtistJson = """
@@ -45,7 +45,7 @@ public class ArtistRestControllerTest {
 	}
 
 	@Test
-	public void testUpdateArtistExisting() throws Exception {
+	void testUpdateArtistExisting() throws Exception {
 		when(artistService.updateArtistById(anyLong(), any(Artist.class)))
 				.thenReturn(new Artist(1L, "John Doe Jr.", "Canadian"));
 
@@ -57,7 +57,7 @@ public class ArtistRestControllerTest {
 	}
 
 	@Test
-	public void testUpdateArtistNotFound() throws Exception {
+	void testUpdateArtistNotFound() throws Exception {
 		when(artistService.updateArtistById(anyLong(), any(Artist.class))).thenReturn(null);
 
 		String updateJson = "{\"name\":\"Nobody\",\"nationality\":\"None\"}";
@@ -67,7 +67,7 @@ public class ArtistRestControllerTest {
 	}
 
 	@Test
-	public void testDeleteArtist() throws Exception {
+	void testDeleteArtist() throws Exception {
 		doNothing().when(artistService).deleteArtistById(anyLong());
 
 		this.mvc.perform(delete("/api/artists/1")).andExpect(status().isOk()).andExpect(content().string(""));
